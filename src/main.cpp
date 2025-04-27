a31423b977b7acd94debcb0275af3475d8e1a62f
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-27 11:37:15 -0500

    ejercicio 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
-//prende led con interruptor D
+//prende led con interruptor D(eje1)
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #define F_CPU 16000000UL
@@ -34,7 +34,7 @@ int main(void){
 */
 
 /*
-//sube baja con interrupcion d
+//sube baja con interrupcion d (eje2)
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #define F_CPU 16000000UL
@@ -88,7 +88,7 @@ int main(void) {
 
 */
 /*
-//interrupcion b
+//interrupcion b (eje3
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -97,15 +97,15 @@ int main(void) {
 
 
 void config_PCI(void){
-  PCICR|=0X01;
+  PCICR|=0X01; //pinb
   PCMSK0|=(0X02);
   DDRB&=~0X02;
   PORTB|= 0X02;
 }
-ISR(PCINT0_vect){
+ISR(PCINT0_vect){ //puerto b  
   _delay_ms(200);
   if(PORTB&0X01){
-    PORTB&=~0X01;
+    PORTB&=~0X01; //pin del b
   }
   else{
       PORTB|=0X01;
@@ -119,4 +119,82 @@ int main(void){
   while(1){
   }
 }
-*/
+  /*
+  void config_PCI(void){
+  PCICR|=0x01;
+  PCMSK0|=0x06;
+  DDRB&=~0x06;
+  PORTB|=0x06;
+}
+ISR(PCINT0_vect){
+  _delay_ms(200);
+  if(PORTB&0x01){
+    PORTB&=~0x01;
+  }
+  else{
+    PORTB|=0x01;
+  }
+}
+int main(void){
+  DDRB|=0x01;
+  PORTB&=~0x01;
+  config_PCI();
+  sei();
+  while(1){
+  } 
+  */ 
+/*
+
+//subir bajar con dos pulsadores usando 2 tipos de interrupciones (eje4)
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+char cuenta=0; //variable global
+
+void config_IE0(void){
+  EIMSK |= (1 << INT0);//puert d
+  EICRA|=(1<<ISC01);
+    
+  DDRD &= ~0x08;  // pulsadores
+  PORTD |= 0x08;  //PULSADORES
+}
+
+ISR(INT0_vect){
+    cuenta++; //aumenta 1 unidad
+    if (cuenta > 99){
+    cuenta = 0; //la suma no va sobre pasar los 99
+    }
+    
+}
+
+ISR(INT1_vect){
+  cuenta--;
+  if(cuenta<0){
+    cuenta=99;
+  }
+}
+
+int main(void) {
+    DDRD |= 0xF0;   // PD4 a PD7 como salida (datos al display)
+    DDRB |= 0x03;   // PB0 y PB1 como salida (selección de display)
+
+    config_IE0();
+    sei();
+    while (1) {
+        
+        PORTB &= ~0x01;     // par apagar el display de decenas
+        PORTB |= 0x02;      // para prender el display de unidades
+        PORTD = (PORTD & 0x0F) | ((cuenta % 10) << 4); // para "desplazar" las unidades a losp ines del deco
+        _delay_ms(8); //display
+
+        // Mostrar decenas
+        PORTB &= ~0x02;     // para apagar el display de unidades
+        PORTB |= 0x01;      // para prender e display de decenas
+        PORTD = (PORTD & 0x0F) | ((cuenta / 10) << 4); 
+        _delay_ms(8);
+  }
+}
+  */
